b718002fe2dc0006b9a6e62f92c2cbbd9634ed05
Idir Lankri <idir.lankri@inria.fr>
2017-07-24 14:52:50 +0200

    Add argument corpus to CGI mkdir_corpus

--- ML/mkdir_corpus_params.ml
+++ ML/mkdir_corpus_params.ml
@@ -2,3 +2,5 @@ value dirname = "dirname"
 ;
 value parent_dir = Params.corpus_dir
 ;
+value mode = Params.corpus_mode
+;
--- ML/mkdir_corpus_params.mli
+++ ML/mkdir_corpus_params.mli
@@ -2,3 +2,5 @@ value dirname : string
 ;
 value parent_dir : string
 ;
+value mode : string
+;

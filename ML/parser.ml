96b32960c5794f30462a3d5a26e471350c656557
Gerard Huet <Gerard.Huet@inria.fr>
2019-04-24 17:10:37 +0200

    patch color pvc pvv + Mnual right url for ML doc

--- ML/parser.ml
+++ ML/parser.ml
@@ -72,7 +72,7 @@ value print_labels tags seg_num = do
 value rec color_of_role = fun (* Semantic role of lexical category *)
   [ Pv | Pvk | Pvkc | Pvkv | Iic | Iic2 | Iik | Voca | Inv | Iicv | Iicc 
   | Iikv | Iikc | Iiif | A | An | Vok | Vokv | Vokc | Vocv | Vocc | Iiy 
-  | Iiv | Iivv | Iivc | Peri | Auxiick -> Grey 
+  | Iiv | Iivv | Iivc | Peri | Auxiick | Pvv | Pvc -> Grey 
   | Noun | Noun2 | Nouv | Nouc | Krid | Kriv | Kric | Pron | Ifc | Ifc2
   | Kama | Lopak | Auxik -> Cyan (* Actor or Predicate *)
   | Root | Lopa |  Auxi -> Pink (* abs-tvaa in Inde *) (* Process *) 

da4e1e6a0fa035327a8f49ba84632ab0be1b6ea5
Gerard Huet <Gerard.Huet@inria.fr>
2021-12-07 16:51:51 +0100

    Interface fix for MW default only when Corpus Reader mode

--- ML/interface.ml
+++ ML/interface.ml
@@ -531,7 +531,8 @@ value graph_engine () = do
     let undo_enabled = sentence_no = "" (* no undo in Reader corpus mode *)
                     || corpus_permission <> Web_corpus.Reader in
     (* TEMPORARY for course: Corpus Reader set to MW lexicon and Deva printing *)
-    let () = if corpus_permission <> Web_corpus.Reader then ()
+    let () = if sentence_no = "" then ()
+             else if corpus_permission <> Web_corpus.Reader then ()
              else do { toggle_lexicon "MW"; toggle_sanskrit_font Deva } in
     let text = arguments translit lex font cache st us url_encoded_input
                          url_encoded_topic abs 

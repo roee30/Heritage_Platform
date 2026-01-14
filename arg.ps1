d9c32a9ec5b89e62afd5d871210a4f7cda7ac712
Roee Nizan <roeen30@gmail.com>
2026-01-14 11:53:42 +0200

    macos compilation

--- arg.ps1
+++ arg.ps1
@@ -4,32 +4,22 @@ param([string]$text)
 
 # set-psdebug -trace 2
 $ErrorActionPreference = "Stop"
-# Ensure script runs in its directory
 Set-Location -LiteralPath $PSScriptRoot
 
-# Define tag function
 function Tag($tag, $content, $extra="") {
     "<$tag $extra>$content</$tag>"
 }
-# Save input
 $text | Set-Content -Encoding UTF8 input.txt
 
-# Process through iast
 chcp 65001 >$null
 $processed = (get-content -encoding utf8 input.txt | & .\iast -a -f input.txt).replace('.a',"'")
 $processed | set-content -encoding utf8 output.txt
-# echo $processed; exit
-# $processed = $text
 
-# Set QUERY_STRING and run interface
 $env:QUERY_STRING = "t=VH&text=$processed"
-# $interfaceOutput = (& ML\interface.exe)
-$interfaceOutput = (& .\_build\install\default\bin\interface.exe)
-# echo $interfaceOutput; exit 1
+#$interfaceOutput = (& .\_build\install\default\bin\interface.exe)
+$interfaceOutput = (& .\interface.exe)
 
-# Read style files
 $style = Get-Content -Raw style.css,tooltip.css
 
-# Output to stdout
 $styleTag = (Tag "style" $style)
 echo "$interfaceOutput`n$styleTag"
